2b46b39028d925eeaef79e5b0611287914a6e5a7
JohnWesley14 <johnwesleyrvaz@gmail.com>
2023-06-17 13:04:52 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { firebase } from '@react-native-firebase/database'
+import database from '@react-native-firebase/database';
+
+
+const reading_sensor = database()
+                      .ref('/Sensor_de_Gas/Valor_de_Leitura')
+                      .on('value', snapshot => {
+                        console.log('Sensor_de_Gas: ', snapshot.val());
+                        var valor = snapshot.val()
+                      });
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>O
+      pp!</Text>
       <StatusBar style="auto" />
     </View>
   );
